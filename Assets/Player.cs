c0b58c78d6d24ecb0606e3b9a0460fd9b48f6b29
soonjeong <woak_uu@naver.com>
2017-04-30 16:42:43 +0900

    레벨 레벨량 게이지상승

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public int level;
+    public int clickup;
+    public int gauge;
+    public int maxgauge;
+}
+
+public class Player : MonoBehaviour
+{
+    public PlayerInfo Info;
+   
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Info.gauge += Info.clickup;
+        }
+      
+        if(Info.gauge>=Info.maxgauge)
+        {
+            Info.level += 1;
+            Info.gauge = 0;
+        }
+	}
+}
---
+++ Assets/gaugeui.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gaugeui : MonoBehaviour
+{
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 size = GetComponent<RectTransform>().sizeDelta;
+        size.x = Player.GetComponent<Player>().Info.gauge;
+        GetComponent<RectTransform>().sizeDelta = size;
+
+    }
+}
